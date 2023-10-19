f9d85837901852ee45cc705acbfacae3d936f545
yassinex02 <tahayassinemoumni@gmail.com>
2023-10-19 16:19:24 +0200

    try

--- openai.py
+++ openai.py
@@ -1,17 +1,7 @@
 import openai
-
-
-conversation = [
-    {"role": "system", "content": "You are a curious football fan."},
-    {"role": "user", "content": "What football team has the most wins in the Champions League?"}
-]
-
-# Make an API call to GPT-3.5 Turbo
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=conversation  # Use 'conversation' here, not 'messages'
-)
-
-# Extract and print the model's reply
-answer = response.choices[0].message['content']
-print("Answer:", answer)
+response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                        messages=[{"role": "system", "content": "You are ahelpful sports assistant."}, {"role": "user", "content": "what is thefootball team that won more Champion leagues?"}])
+result = ''
+for choice in response.choices:
+    result += choice['message']['content']
+print(result)
